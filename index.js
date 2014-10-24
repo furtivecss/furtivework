ae4635ef842d02914c1b8806b286832bf008320c
John Otander <johnotander@gmail.com>
2014-10-23 21:47:30 -0600

    Nuke autoprefixer, bump version.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var rework       = require('rework'),
     reworkNPM    = require('rework-npm'),
-    reworkVars   = require('rework-vars'),
-    autoprefixer = require('autoprefixer');
+    reworkVars   = require('rework-vars');
 
 module.exports = function(src, options) {
-  var css = rework(src, options || {})
-              .use(reworkNPM())
-              .use(reworkVars())
-              .toString();
-
-  return autoprefixer().process(css).css;
+  return rework(src, options || {})
+          .use(reworkNPM())
+          .use(reworkVars())
+          .toString({ sourcemap: true });
 };
